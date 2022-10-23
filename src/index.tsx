593f50849f03c4a8405fb02474ceab8fa7dc1084
Tyrah Gullette <tgullette@tgullet-ltmb9un.internal.salesforce.com>
2022-10-22 18:35:27 -0700

    initial commit for gccbook project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    //ref.current refers to any type of variable
+    const ref = useRef<any>();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+    }
+
+    useEffect(() => {
+        startService();
+    }, [])
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        //transpile and build
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window'
+            }
+        });
+
+        // console.log(result);
+
+        setCode(result.outputFiles[0].text);
+    }
+
+    return <div>
+        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
